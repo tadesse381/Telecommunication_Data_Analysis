2dbb88209466791ab5b9c7ca2fd8ce2862f19975
Tadesse-Kebede <85294225+tadesse381@users.noreply.github.com>
2022-05-13 20:27:07 +0300

    Telocommunication Data Analysis

--- pages/marketing.py
+++ pages/marketing.py
@@ -6,7 +6,41 @@ import scripts.ploting as plot
 
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
+def pareto_plot(df, x=None, y=None, title=None, show_pct_y=False, pct_format='{0:.0%}'):
+    xlabel = x
+    ylabel = y
+    tmp = df.sort_values(y, ascending=False)
+    x = tmp[x].values
+    y = tmp[y].values
+    weights = y / y.sum()
+    cumsum = weights.cumsum()
+    
+    fig, ax1 = plt.subplots()
+    ax1.bar(x, y)
+    ax1.set_xlabel(xlabel)
+    ax1.set_ylabel(ylabel)
 
+    ax2 = ax1.twinx()
+    ax2.plot(x, cumsum, '-ro', alpha=0.5)
+    ax2.set_ylabel('', color='r')
+    ax2.tick_params('y', colors='r')
+    
+    vals = ax2.get_yticks()
+    ax2.set_yticklabels(['{:,.2%}'.format(x) for x in vals])
+
+    # hide y-labels on right side
+    if not show_pct_y:
+        ax2.set_yticks([])
+    
+    formatted_weights = [pct_format.format(x) for x in cumsum]
+    for i, txt in enumerate(formatted_weights):
+        ax2.annotate(txt, (x[i], cumsum[i]), fontweight='heavy')    
+    
+    if title:
+        plt.title(title)
+    
+    plt.tight_layout()
+    plt.show()
 
 def run_marketing():
   st.write("## Marketing Analysis")
@@ -16,7 +50,7 @@ def run_marketing():
   
   top_10_handset = df_clean.groupby("Handset Type")['MSISDN/Number'].nunique().nlargest(10)
   top_3_manufacturers = df_clean.groupby("Handset Manufacturer")['MSISDN/Number'].nunique().nlargest(3)
-
+  pareto_plot(top_3_manufacturers, x='MSISDN/Number', y='Handset Manufacturer', title='Top 3 handset manufacturers')
 
   fig, (ax1, ax2) = plt.subplots(1, 2,figsize=(12,7))
 
--- scripts/ploting.py
+++ scripts/ploting.py
@@ -61,7 +61,41 @@ def plot_scatter(df: pd.DataFrame, x_col: str, y_col: str, title: str, hue: str,
     plt.xticks(fontsize=14)
     plt.yticks( fontsize=14)
     plt.show()
+def pareto_plot(df, x=None, y=None, title=None, show_pct_y=False, pct_format='{0:.0%}'):
+    xlabel = x
+    ylabel = y
+    tmp = df.sort_values(y, ascending=False)
+    x = tmp[x].values
+    y = tmp[y].values
+    weights = y / y.sum()
+    cumsum = weights.cumsum()
+    
+    fig, ax1 = plt.subplots()
+    ax1.bar(x, y)
+    ax1.set_xlabel(xlabel)
+    ax1.set_ylabel(ylabel)
 
+    ax2 = ax1.twinx()
+    ax2.plot(x, cumsum, '-ro', alpha=0.5)
+    ax2.set_ylabel('', color='r')
+    ax2.tick_params('y', colors='r')
+    
+    vals = ax2.get_yticks()
+    ax2.set_yticklabels(['{:,.2%}'.format(x) for x in vals])
+
+    # hide y-labels on right side
+    if not show_pct_y:
+        ax2.set_yticks([])
+    
+    formatted_weights = [pct_format.format(x) for x in cumsum]
+    for i, txt in enumerate(formatted_weights):
+        ax2.annotate(txt, (x[i], cumsum[i]), fontweight='heavy')    
+    
+    if title:
+        plt.title(title)
+    
+    plt.tight_layout()
+    plt.show()
 def serious_bar(serious, ax):
     '''
     Plot bar chart for serious data types
