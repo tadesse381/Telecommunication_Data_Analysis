06a45fcb4fea126d10c9c42bc54c4a870a76e0eb
Tadesse-Kebede <85294225+tadesse381@users.noreply.github.com>
2022-05-14 17:17:48 +0300

    Telocommunication Data Analysis Last Date Commit

--- pages/satisfaction.py
+++ pages/satisfaction.py
@@ -30,7 +30,7 @@ def run_satisfaction():
   df_normalized = pd.DataFrame(data=scalled_values, columns=df_task2.columns)
   kmeans = KMeans(n_clusters=3).fit(df_normalized)
   #Read the cleaned csv file and store it on data
-  file_name = 'data/clean_telecommunication_data.csv'
+  file_name = 'data/Week1_challenge_data_source.csv'
   data = pd.read_csv(file_name)
   df_task_3 = pd.read_csv(file_name)
   new_netwok_df = df_task_3[['MSISDN/Number', 'Handset Type','TCP DL Retrans. Vol (Bytes)', 'TCP UL Retrans. Vol (Bytes)',\
