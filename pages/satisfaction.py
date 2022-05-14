3e8d9383732fa62f52dbb388dc7a7dc6b7e30652
Tadesse-Kebede <85294225+tadesse381@users.noreply.github.com>
2022-05-14 17:01:09 +0300

    Telocommunication Data Analysis Last Date Commit

--- pages/satisfaction.py
+++ pages/satisfaction.py
@@ -42,6 +42,12 @@ def run_satisfaction():
   cluster_0 = cluster_group_df.get_group(0)
   cluster_1 = cluster_group_df.get_group(1)
   cluster_2 = cluster_group_df.get_group(2)
+  ## First normalize the Data, Then Cluster
+  min_max_scaler = preprocessing.MinMaxScaler()
+  network_values = net_cluster_df.values
+  scalled_values = min_max_scaler.fit_transform(network_values)
+  df_network_normalized = pd.DataFrame(data=scalled_values, columns=df_task2.columns)
+  kmeans = KMeans(n_clusters=3).fit(df_normalized)
   ## Engagement Score
   lowest_engagement = engagement_df.groupby('cluster-engagement').get_group(0).mean()
   st.write(lowest_engagement)
