9c0d4320e88ca56eae4109745c21b499049a046b
Tadesse-Kebede <85294225+tadesse381@users.noreply.github.com>
2022-05-14 16:16:45 +0300

    Telocommunication Data Analysis#

--- pages/satisfaction.py
+++ pages/satisfaction.py
@@ -11,26 +11,6 @@ from sklearn.cluster import KMeans
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler
 def run_satisfaction():
-  st.write("## User Satisfaction Analysis")
-  file_name = 'data/clean_telecommunication_data.csv'
-  data = pd.read_csv(file_name)
-  df_clean = data.copy()
-  aggregate = {"Bearer Id": 'count', 'Dur. (ms).1':'sum', 'Total UL (Bytes)': 'sum', 'Total DL (Bytes)': 'sum'}
-  aggregation_result = df_clean.groupby('MSISDN/Number').agg(aggregate)
-  aggregation_result.head()
-  df_task2 = df_clean.copy()
-  df_task2['Total'] = df_task2['Total UL (Bytes)'] + df_task2['Total DL (Bytes)']
-  df_task2 = df_task2.groupby('MSISDN/Number')\
-           .agg({"Bearer Id": "count", 'Dur. (ms).1':'sum', 'Total':'sum'})
-  df_task2.head()
-  min_max_scaler = preprocessing.MinMaxScaler()
-  df_values = df_task2.values
-  scalled_values = min_max_scaler.fit_transform(df_values)
-  df_normalized = pd.DataFrame(data=scalled_values, columns=df_task2.columns)
-  kmeans = KMeans(n_clusters=3).fit(df_normalized)
-  cluster = kmeans.predict(df_normalized)
-  engagement_df = df_task2.copy()
-  engagement_df['cluster-engagement']  = cluster
   ## Engagement Score
   lowest_engagement = engagement_df.groupby('cluster-engagement').get_group(0).mean()
   st.write(lowest_engagement)
