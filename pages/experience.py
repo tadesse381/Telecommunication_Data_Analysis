0bb31df9ce2ac408cd1587c4ebc0d337ed4af7cf
Tadesse-Kebede <85294225+tadesse381@users.noreply.github.com>
2022-05-14 08:01:15 +0300

    Telocommunication Data Analysis#

--- pages/experience.py
+++ pages/experience.py
@@ -1,5 +1,21 @@
 import streamlit as st
-
+# My Custom Modules Importing
+from scripts import file
+def null_percentage(df):
+    number_of_rows, number_of_columns = df.shape
+    df_size = number_of_rows * number_of_columns
+    
+    null_size = (df.isnull().sum()).sum()
+    percentage = round((null_size / df_size) * 100, 2)
+    print(f"Data Fraame contain null values of { percentage }%")
 def run_experiance():
-  st.write("## User Experiance Analysis")
+#Read the cleaned csv file and store it on data
+file_name = 'Week1_challenge_data_source.csv'
+df_task_3 = file.read_csv(file_name)
+new_netwok_df = df_task_3[['MSISDN/Number', 'Handset Type','TCP DL Retrans. Vol (Bytes)', 'TCP UL Retrans. Vol (Bytes)',\
+                         'Avg RTT DL (ms)', 'Avg RTT UL (ms)',\
+                         'Avg Bearer TP DL (kbps)', 'Avg Bearer TP UL (kbps)']]
+null_percentage(new_netwok_df)
+new_netwok_df.isnull().sum()
+st.write("## User Experiance Analysis")
   
