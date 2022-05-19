c7f15bb3542465b3ba4e3d64294d9e41ab5ff320
Tadesse-Kebede <85294225+tadesse381@users.noreply.github.com>
2022-05-19 17:54:54 +0300

    Test on Data Set

---
+++ tests/test_dataset.py
@@ -0,0 +1,81 @@
+import unittest
+
+import pandas as pd
+
+
+class TestDataframe(unittest.TestCase):
+    def setUp(self) -> pd.DataFrame:
+        try:
+            data = pd.read_csv(
+                "../data/clean_telecommunication_data.csv", parse_dates=["Start", "End"]
+            )
+            self.dataframe = data
+        except IOError:
+            print("could not open csv file")
+
+    def test_dataframe_shape(self):
+        self.assertEqual(self.dataframe.shape, (148506, 55))
+
+    def test_columns(self):
+        dataset_columns = [
+            "Bearer Id",
+            "Start",
+            "Start ms",
+            "End",
+            "End ms",
+            "Dur. (ms)",
+            "IMSI",
+            "MSISDN/Number",
+            "IMEI",
+            "Last Location Name",
+            "Avg RTT DL (ms)",
+            "Avg RTT UL (ms)",
+            "Avg Bearer TP DL (kbps)",
+            "Avg Bearer TP UL (kbps)",
+            "TCP DL Retrans. Vol (Bytes)",
+            "TCP UL Retrans. Vol (Bytes)",
+            "DL TP < 50 Kbps (%)",
+            "50 Kbps < DL TP < 250 Kbps (%)",
+            "250 Kbps < DL TP < 1 Mbps (%)",
+            "DL TP > 1 Mbps (%)",
+            "UL TP < 10 Kbps (%)",
+            "10 Kbps < UL TP < 50 Kbps (%)",
+            "50 Kbps < UL TP < 300 Kbps (%)",
+            "UL TP > 300 Kbps (%)",
+            "HTTP DL (Bytes)",
+            "HTTP UL (Bytes)",
+            "Activity Duration DL (ms)",
+            "Activity Duration UL (ms)",
+            "Dur. (ms).1",
+            "Handset Manufacturer",
+            "Handset Type",
+            "Nb of sec with 125000B < Vol DL",
+            "Nb of sec with 1250B < Vol UL < 6250B",
+            "Nb of sec with 31250B < Vol DL < 125000B",
+            "Nb of sec with 37500B < Vol UL",
+            "Nb of sec with 6250B < Vol DL < 31250B",
+            "Nb of sec with 6250B < Vol UL < 37500B",
+            "Nb of sec with Vol DL < 6250B",
+            "Nb of sec with Vol UL < 1250B",
+            "Social Media DL (Bytes)",
+            "Social Media UL (Bytes)",
+            "Google DL (Bytes)",
+            "Google UL (Bytes)",
+            "Email DL (Bytes)",
+            "Email UL (Bytes)",
+            "Youtube DL (Bytes)",
+            "Youtube UL (Bytes)",
+            "Netflix DL (Bytes)",
+            "Netflix UL (Bytes)",
+            "Gaming DL (Bytes)",
+            "Gaming UL (Bytes)",
+            "Other DL (Bytes)",
+            "Other UL (Bytes)",
+            "Total UL (Bytes)",
+            "Total DL (Bytes)",
+        ]
+        self.assertEqual(self.dataframe.columns, dataset_columns)
+
+
+if __name__ == "__main__":
+    unittest.main()
